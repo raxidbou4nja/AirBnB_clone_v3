860e2919dc66d135060b269e1462e4d0edca7b32
raxidbou4nja <raxid.bou4nja@gmail.com>
2024-02-04 17:07:49 -0800

    add new files db and file storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -15,7 +15,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from models import State
+from models.state import State
 
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
